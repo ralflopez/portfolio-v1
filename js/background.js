8dae7c0c07b87b5030ddee2a349fded5faa5efcc
Ralf Lopez <ralflopez3@gmail.com>
2021-03-21 19:31:03 +0800

    set canvas background

---
+++ js/background.js
@@ -0,0 +1,70 @@
+// canvas config
+const canvas = document.getElementById('canvas');
+const c = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// adapt to screen change
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+// circle
+class Circle {
+    constructor(rad, dy) {
+        this.rad = rad;
+        this.x = Math.random() * innerWidth;
+        // this.y = innerHeight + rad;
+        this.y = Math.random() * innerHeight + innerHeight;
+        this.dy = dy;
+        this.colors = ['rgb(249, 240,157)', 'rgb(168, 231,167)', 'rgb(157,244,249)'];
+        this.color = this.colors[Math.floor(Math.random() * this.colors.length)];
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.rad, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    update() {
+        if(this.y + this.rad < 0) {
+            this.y = Math.random() * innerHeight + innerHeight;
+            this.x = Math.random() * innerWidth;
+            this.color = this.colors[Math.floor(Math.random() * this.colors.length)];
+        }
+        else
+            this.y += this.dy;
+        this.draw();
+    }
+}
+
+// background
+class BubblesCanvas {
+    constructor(n) {
+        this.n = n;
+        this.bubbles = [];
+        this.init();
+    }
+
+    init() {
+        for(let i = 0; i < this.n; i++) {
+            this.bubbles.push(new Circle(4, -1.2));
+        }
+    }
+}
+
+// declare background
+const bubble_bg = new BubblesCanvas(25);
+
+// auto run animate function
+(function animate() {
+    requestAnimationFrame(animate);
+
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    
+    for(let i = 0; i < bubble_bg.bubbles.length; i++)
+        bubble_bg.bubbles[i].update();
+})();
